ef229f3348972aa92ed581099a8a781f072a1735
Linux Laptop <kylewatson98@gmail.com>
2016-12-22 18:52:34 +0000

    cleaned up scripts

--- index.php
+++ index.php
@@ -6,30 +6,15 @@
     <meta name="keywords" content="Portfolio, Student, Developer, Counter-Strike">
     <meta name="description" content="A simple portfolio to show off projects and attributes.">
     <meta name="author" content="Kyle Watson">
-    <!--Font Awesome by Dave Gandy - http://fontawesome.io-->
-    <!-- <i> tags used for glyphicons (styling) so technically incorrect use of HTML but hidden from accessibility with "aria-hidden=true" attribute-->
-    <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
     <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-    <script type="text/javascript" src="jquery.p2r.js" defer></script>
     <script type="text/javascript" src="script.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
 <main id="bio" tabindex="-1">
-    <!--TODO: Change to jquery mobile layout and add each day as a diff page so i can slide to each -->
-    <!-- Content starts here -->
-    <!--<div id="pullDown"> Pull to refresh </div>
-    <section id="top">
-
-    </section>
-    <section id="mainsec">
-        <div id="bottom">
-        </div>
-    </section>-->
-
     <div id="Mon" data-role="page" >
         <div data-role="header">
             <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-bars ui-btn-icon-left ui-btn-icon-notext">Home</a>
