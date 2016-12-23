b72dd48571d7dad55af42833dbfe7e548bc56d46
Linux Laptop <kylewatson98@gmail.com>
2016-12-23 15:32:30 +0000

    fixed parsing json database saving now works except not all mirror text works (lots of testing to start

--- date.php
+++ date.php
@@ -19,7 +19,6 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
 
     // use exec() because no results are returned
     foreach ($conn->query($sql) as $row) {
